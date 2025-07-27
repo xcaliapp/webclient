523ff89a32958c2cb4bb70b4fdd382fadc83f683
Kovács Péter <peter.dunay.kovacs@gmail.com>
2025-07-27 13:05:00 +0200

    Fix code style

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,10 +1,10 @@
-import type { RenderOptions } from "@testing-library/react"
-import { render } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import type { PropsWithChildren, ReactElement } from "react"
-import { Provider } from "react-redux"
-import type { AppStore, RootState } from "../app/store"
-import { makeStore } from "../app/store"
+import type { RenderOptions } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import type { PropsWithChildren, ReactElement } from "react";
+import { Provider } from "react-redux";
+import type { AppStore, RootState } from "../app/store";
+import { makeStore } from "../app/store";
 
 /**
  * This type extends the default options for
@@ -43,23 +43,23 @@ interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
  */
 export const renderWithProviders = (
   ui: ReactElement,
-  extendedRenderOptions: ExtendedRenderOptions = {},
+  extendedRenderOptions: ExtendedRenderOptions = {}
 ) => {
   const {
     preloadedState = {},
     // Automatically create a store instance if no store was passed in
     store = makeStore(preloadedState),
     ...renderOptions
-  } = extendedRenderOptions
+  } = extendedRenderOptions;
 
   const Wrapper = ({ children }: PropsWithChildren) => (
     <Provider store={store}>{children}</Provider>
-  )
+  );
 
   // Return an object with the store and all of RTL's query functions
   return {
     store,
     user: userEvent.setup(),
-    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
-  }
-}
+    ...render(ui, { wrapper: Wrapper, ...renderOptions })
+  };
+};
