175ef0b7eadcbf3eb928e0a85efb51ba597cd050
Kovács Péter <peter.dunay.kovacs@gmail.com>
2025-08-10 18:43:31 +0200

    Add missing import

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { JSX, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
